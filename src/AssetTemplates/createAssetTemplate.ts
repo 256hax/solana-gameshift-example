36d31c934eb4321ed50ae99c2d671e2d2ceeb98c
256hax <256hax@gmail.com>
2024-03-10 17:16:12 +0900

    init

---
+++ src/AssetTemplates/createAssetTemplate.ts
@@ -0,0 +1,5 @@
+// You can create the Asset Template using dashboard of GameShift.
+// https://app.gameshift.dev/
+// 
+// If you need to make a script, check here.
+// https://docs.gameshift.dev/reference/assettemplatecontroller_create
