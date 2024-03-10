342a0a421d67668ed9f33a19fef84db3a5b81d16
256hax <256hax@gmail.com>
2024-03-10 15:45:33 +0900

    chore: change test id/wallet

--- src/assets/transferAsset.ts
+++ src/assets/transferAsset.ts
@@ -7,14 +7,14 @@ export const createAsset = async () => {
   const gameshiftApiKey = process.env.GAMESHIFT_API_KEY;
   if (!gameshiftApiKey) throw new Error('gameshiftApiKey not found.');
 
-  const assetId = 'bb80f3f7-4b70-46a1-befa-4e3c6b686243'; // Specifies the Asset to transfer
+  const assetId = '826ea05f-c62c-4e62-9d0e-0eba87ca0eaa'; // Specifies the Asset to transfer
   const url = `https://api.gameshift.dev/assets/${assetId}/transfer`;
 
   // You must provide *either* a destination *or* a wallet, not both.
   const data = {
-    onBehalfOf: '47e33b63-ad72-4968-aace-f5ce0e3fb3a8', // Identifies the User tranferring the Asset
+    onBehalfOf: '132bc999-b3ef-4de5-8265-3c72b7c69cd4', // Identifies the User tranferring the Asset
     // destinationUserReferenceId: '132bc999-b3ef-4de5-8265-3c72b7c69cd4', // Identifies the User receiving the Asset
-    destinationWallet: 'AUf9whyJe3eKB6A5kqk6eFHNW7gsyqLygnwNAjDYoXDF', // Address of the wallet receiving the Asset
+    destinationWallet: 'HXtBm8XZbxaTt41uqaKhwUAa6Z1aPyvJdsZVENiWsetg', // Address of the wallet receiving the Asset
   };
 
   const config = {
@@ -37,5 +37,5 @@ createAsset();
 /*
 % ts-node src/<THIS_FILE>
 
-https://app.gameshift.dev/consent?transaction=944e6fe0-2d85-4160-b84b-e723428cc51e
+https://app.gameshift.dev/consent?transaction=86ac9435-8998-4614-ab98-ecedd9a846a5
 */
